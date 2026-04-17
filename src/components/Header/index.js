635c97602c983039d855365367abd4df39c3a3a1
angeltufino07 <angeltufino.07@gmail.com>
2026-04-17 15:09:39 -0400

    Refine Header component styling/navigation

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,67 +1,42 @@
-import React,{ useState } from "react";
+import React from "react";
 import { NavLink } from "react-router-dom";
-import logo from "../../assets/logo.png"
-import Menu from "../../assets/Menu.png"
-import Close from "../../assets/Close.png"
-import "./styles.css"
-
-
-
-
-
+import logo from "../../assets/logo.png";
+import "./styles.css";
 
 const Header = () => {
-  
   return (
-
-  <div className="container">  
-  
-    <div className="header">
-        <div class="header-container">  
-                <div id="logo">
-                    <a href="#">
-                        <img className="header-logo" alt="logo principal" src={logo} />
-                    </a> 
-                </div>
-                <nav class="navbar container" data-navbar>
-        <ul class="navbar-list">
-
-          <li >
-            <a href="/" class="navbar-link" data-nav-link>Home</a>
-          </li>
-
-          <li >
-            <a href="/#services" class="navbar-link" data-nav-link>Organize your Travel</a>
-          </li>
-
-          <li >
-            <a href="/#Comission" class="navbar-link" data-nav-link>Bussines</a>
-          </li>
-
-          <li >
-            <a href="/#reviews" class="navbar-link" data-nav-link>Reviews</a>
-          </li>
-
-          <li >
-            <a href="/travels" class="navbar-link" data-nav-link>Travels</a>
-          </li>
-          
-          <li >
-            <a href="/about" class="navbar-link" data-nav-link>About Us</a>
-          </li>
-        </ul>
-      </nav>
-
-
-
- 
-      
- 
+    <div className="container">
+      <div className="header">
+        <div className="header-container">
+          <div id="logo">
+            <NavLink to="/">
+              <img className="header-logo" alt="Travel Agency logo" src={logo} />
+            </NavLink>
+          </div>
+          <nav className="navbar container">
+            <ul className="navbar-list">
+              <li>
+                <NavLink to="/" className="navbar-link">Home</NavLink>
+              </li>
+              <li>
+                <NavLink to="/#services" className="navbar-link">Organize your Travel</NavLink>
+              </li>
+              <li>
+                <NavLink to="/#Comission" className="navbar-link">Bussines</NavLink>
+              </li>
+              <li>
+                <NavLink to="/#reviews" className="navbar-link">Reviews</NavLink>
+              </li>
+              <li>
+                <NavLink to="/travels" className="navbar-link">Travels</NavLink>
+              </li>
+              <li>
+                <NavLink to="/about" className="navbar-link">About Us</NavLink>
+              </li>
+            </ul>
+          </nav>
         </div>
-    </div>
-
-
-    
+      </div>
     </div>
   );
 };
